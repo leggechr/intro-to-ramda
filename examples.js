71a9f8a3156839e7e35dd9ff5ff7947f0df86217
Christine Legge <christine@axiomzen.co>
2016-08-11 11:28:14 -0700

    update examples to match slides

--- examples.js
+++ examples.js
@@ -1,65 +1,81 @@
-// Use node v4.4.7 or higher 
+// Use node v4.4.7 or higher
 // Install ramda before running using the command `npm install ramda`
+// Then run this file using the command `node --harmony examples.js`
+
 var R = require('ramda');
 
-var myList = [1, 2, 3, 4, 5]
-
-function bewareOfMutatingVariables(options) {
-  var moreOptions = _.extend(options, {addedProp: 'addedProp'});
-
-  Service.doSomething(options); // BEWARE!! options is no longer what you think it is 😱
-  Service.doSomethingElse(moreOptions);
+// not functional / not pure version of increment
+var a1 = 0;
+function increment() {
+  a1 += 1;
 }
+increment();
+increment();
+console.log(a1); // a1 has been changed
 
-// slice is a pure function, these next 3 lines will not mutate myList
-myList.slice(0,3); // => [1, 2, 3]
-myList.slice(0,3); // => [1, 2, 3]
-myList.slice(0,3); // => [1, 2, 3]
-console.log(myList);
-
-// splice is NOT a pure function, these next 3 lines will mutate myList
-myList.splice(0,3); // => [1, 2, 3]
-myList.splice(0,3); // => [4, 5]
-myList.splice(0,3); // => []
-console.log(myList);
-
-// simple add function
-function add(a, b) {
-  return a + b;
+// function / pure version of increment
+var a = 0;
+function increment(a2) {
+  return a2 + 1;
 }
-console.log(add(3));
-console.log(add(3, 4));
+console.log(increment(a));
+console.log(increment(a));
+console.log(a); // a remains unchanged
 
-// curried add function
-function curriedAdd(a) {
-  return function(b) {
-    return a + b;
-  }
-}
-var add3 = curriedAdd(3); // => returns a function that accepts the argument b
-console.log(add3(4)); // => 7
+var myObj = { id: 1, isOn: false };
 
-// curried add function using Ramda
-var ramdaAdd3 = R.add(3);
-console.log(ramdaAdd3(4)); // => 7
-
-var myObj = {
-  id: 1,
-  name: 'i_love_ramda',
-  status: false
-};
-
-// return a new copy of an obj with an updated property
-function toggleStatus(obj) {
+// enable function using javascript (ES6)
+function enable(obj) {
   return Object.assign({}, obj, {
-    status: true
+    isOn: true
   });
 }
-console.log(toggleStatus(myObj));
 
-// the same function above with Ramda
-var toggleStatus = R.assoc('status', true);
-console.log(toggleStatus(myObj));
+// same enable function using Ramda
+var ramdaEnable = R.assoc('isOn', true);
 
-//the original object remains unchanged
-console.log(myObj);
+var newObj = enable(myObj);
+var ramdaNewObj = ramdaEnable(myObj);
+console.log(newObj);
+console.log(ramdaNewObj);
+console.log(myObj); // myObj remains unchanged
+
+var originalState = {
+  todos: [
+    {
+      text: 'learn ramda',
+      complete: false
+    },
+    {
+      text: 'learn functional programming',
+      complete: false
+    }
+  ]
+};
+
+var myAction = {
+  text: 'learn haskell'
+};
+
+// a redux reducer using javascript (ES6)
+function addToDo(state, action) {
+  var newTodo = { text: action.text, complete: false };
+  var todos = [ ...state.todos, newTodo ];
+
+  return Object.assign({}, state, {
+    todos: todos
+  });
+}
+
+// the same redux reducer using Ramda
+function ramdaAddToDo(state, action) {
+  var newTodo = { text: action.text, complete: false };
+  var todos = R.append(newTodo, state.todos);
+  return R.assoc('todos', todos, state);
+}
+
+var newState = addToDo(originalState, myAction);
+var ramdaNewState = ramdaAddToDo(originalState, myAction);
+console.log(newState);
+console.log(ramdaNewState);
+console.log(originalState); // original state remains unchanged
